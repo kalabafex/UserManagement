a8965918443edc311b6baf237758de3f1f4d4be8
kalab Afera <kalabafex@gmail.com>
2024-12-02 21:19:22 -0600

    role

--- src/main/java/com/kai/Registration/repository/UserRepository.java
+++ src/main/java/com/kai/Registration/repository/UserRepository.java
@@ -2,8 +2,11 @@ package com.kai.Registration.repository;
 
 import com.kai.Registration.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
+    @Query("SELECT u FROM User u LEFT JOIN FETCH u.roles WHERE u.email = :email")
+    User findByEmail(@Param("email") String email);
 }
 
